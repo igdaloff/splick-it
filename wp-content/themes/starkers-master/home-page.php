319b83005088b25ca99d492fda6f7536918c8378
igdaloff <louis.nathan@gmail.com>
2013-09-04 14:07:45 -0400

    Beginning to add sub-section content and styles

--- wp-content/themes/starkers-master/home-page.php
+++ wp-content/themes/starkers-master/home-page.php
@@ -17,4 +17,32 @@ Template Name: home
 		</ul>
 	</div>
 </div>
+
+<section class="sub-section first-sub-section">
+	<div class="sub-section-header">
+		<div class="wrapper">
+			<h2>Custom Apps</h2>
+			<a class="learn-more-button" href="">Learn More</a>
+		</div>
+	</div>
+	<div class="sub-section-body">
+		<ul>
+			<li>
+				<img src="" alt="" />
+				<h3>Easy To Setup Your Brand</h3>
+				<p>Our robust & scalable platform makes it fast & easy to set up your branded app whether you have 1 store or 1000’s. It’s all about you!</p>
+			</li>
+			<li>
+				<img src="" alt="" />
+				<h3>Industry Leading Technology</h3>
+				<p>Your website and apps are designed and built by award-winning team of technologists and will always be fresh, updated and mobile compatible.</p>
+			</li>
+			<li>
+				<img src="" alt="" />
+				<h3>Multiple Delivery Options</h3>
+				<p>One size does not fit all. Splick-it lets you receive orders and data the way that works best for you business via POS, Fax, Printer, Tablet, Phone.</p>
+			</li>
+		</ul>
+	</div>
+</section>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>

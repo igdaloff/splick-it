ed634e3ba40946b27569c3fc347d6fc406db6c7b
igdaloff <louis.nathan@gmail.com>
2013-09-04 23:41:58 -0400

    Initial email input section and footer css

--- wp-content/themes/starkers-master/home-page.php
+++ wp-content/themes/starkers-master/home-page.php
@@ -22,23 +22,25 @@ Template Name: home
 	<div class="sub-section-header">
 		<div class="wrapper">
 			<h2>Custom Apps</h2>
-			<a class="learn-more-button button" href="">Learn More</a>
+			<a class="learn-more-button-desktop button" href="">Learn More</a>
+			<a class="learn-more-button-mobile button" data-icon="+"></a>
+			<span class="screen-reader-text">Learn More</span>
 		</div>
 	</div>
 	<div class="sub-section-body">
 		<div class="wrapper">
 			<ul>
-				<li>
+				<li class="sub-section-item">
 					<img class="sub-section-icon" src="<?php bloginfo('template_url'); ?>/images/sub-section/tv.png" alt="TV icon" />
 					<h3>Easy To Setup Your Brand</h3>
 					<p>Our robust & scalable platform makes it fast & easy to set up your branded app whether you have 1 store or 1000’s. It’s all about you!</p>
 				</li>
-				<li>
+				<li class="sub-section-item">
 					<img class="sub-section-icon" src="<?php bloginfo('template_url'); ?>/images/sub-section/computer.png" alt="TV icon" />
 					<h3>Industry Leading Technology</h3>
 					<p>Your website and apps are designed and built by award-winning team of technologists and will always be fresh, updated and mobile compatible.</p>
 				</li>
-				<li>
+				<li class="sub-section-item last-sub-section-item">
 					<img class="sub-section-icon" src="<?php bloginfo('template_url'); ?>/images/sub-section/box.png" alt="TV icon" />
 					<h3>Multiple Delivery Options</h3>
 					<p>One size does not fit all. Splick-it lets you receive orders and data the way that works best for you business via POS, Fax, Printer, Tablet, Phone.</p>
@@ -52,23 +54,25 @@ Template Name: home
 	<div class="sub-section-header">
 		<div class="wrapper">
 			<h2>Effective Platform</h2>
-			<a class="learn-more-button button" href="">Learn More</a>
+			<a class="learn-more-button-desktop button" href="">Learn More</a>
+			<a class="learn-more-button-mobile button" data-icon="+"></a>
+			<span class="screen-reader-text">Learn More</span>
 		</div>
 	</div>
 	<div class="sub-section-body">
 		<div class="wrapper">
 			<ul>
-				<li>
+				<li class="sub-section-item">
 					<img class="sub-section-icon" src="<?php bloginfo('template_url'); ?>/images/sub-section/tickets.png" alt="Tickets icon" />
 					<h3>Higher Ticket Average</h3>
 					<p>The average Splick-it order is 30% higher than walk-in orders with engaged restaurants that implement best practices. Boom!</p>
 				</li>
-				<li>
+				<li class="sub-section-item">
 					<img class="sub-section-icon" src="<?php bloginfo('template_url'); ?>/images/sub-section/headphones.png" alt="Headphones icon" />
 					<h3>24/7 Customer Support</h3>
 					<p>Our team is known for it’s awesome customer support. We’re there for you every step of the way. We are here for you!</p>
 				</li>
-				<li>
+				<li class="sub-section-item last-sub-section-item">
 					<img class="sub-section-icon" src="<?php bloginfo('template_url'); ?>/images/sub-section/smiley.png" alt="Smiley face icon" />
 					<h3>Super Happy Customers</h3>
 					<p>People love our apps (and yours), they are consistently ranked higher in the app store because of our great user experience.</p>
@@ -82,29 +86,43 @@ Template Name: home
 	<div class="sub-section-header">
 		<div class="wrapper">
 			<h2>Grow Your Business</h2>
-			<a class="learn-more-button button" href="">Learn More</a>
+			<a class="learn-more-button-desktop button" href="">Learn More</a>
+			<a class="learn-more-button-mobile button" data-icon="+"></a>
+			<span class="screen-reader-text">Learn More</span>
 		</div>
 	</div>
 	<div class="sub-section-body">
 		<div class="wrapper">
 			<ul>
-				<li>
+				<li class="sub-section-item">
 					<img class="sub-section-icon" src="<?php bloginfo('template_url'); ?>/images/sub-section/network.png" alt="Network icon" />
 					<h3>Distribution Network</h3>
 					<p>Our robust & scalable platform makes it fast & easy to set up your branded app whether you have 1 store or 1000’s. It’s all about you!</p>
 				</li>
-				<li>
+				<li class="sub-section-item">
 					<img class="sub-section-icon" src="<?php bloginfo('template_url'); ?>/images/sub-section/chart.png" alt="Chart icon" />
 					<h3>Marketing Tools & Analytics</h3>
 					<p>Your website and apps are designed and built by award-winning team of technologists and will always be fresh, updated and mobile compatible.</p>
 				</li>
-				<li>
+				<li class="sub-section-item last-sub-section-item">
 					<img class="sub-section-icon" src="<?php bloginfo('template_url'); ?>/images/sub-section/calendar.png" alt="Calendar icon" />
 					<h3>Account Managers</h3>
 					<p>One size does not fit all. Splick-it lets you receive orders and data the way that works best for you business via POS, Fax, Printer, Tablet, Phone.</p>
 				</li>
 			</ul>
 		</div>
+	</div>
+</section>
+
+<section class="newsletter">
+	<div class="wrapper">
+		<form>
+			<label>Sign up to receive our weekly newsletter on mobile&nbsp;commerce:</label>
+			<div class="email-input">
+				<input type="text">
+				<button type="submit">Go</button>
+			</div>
+		</form>
 	</div>
 </section>
 

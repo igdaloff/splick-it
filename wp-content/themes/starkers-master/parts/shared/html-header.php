45b2e68b605dcfae4926b79919afb226acc13598
igdaloff <louis.nathan@gmail.com>
2013-09-09 17:45:29 -0400

    Fixing secondary page p and img widths and adding GA

--- wp-content/themes/starkers-master/parts/shared/html-header.php
+++ wp-content/themes/starkers-master/parts/shared/html-header.php
@@ -15,6 +15,21 @@
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
 		<link rel="stylesheet" type="text/css" href="//cloud.typography.com/6961452/726442/css/fonts.css" />
 		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/style.css" type="text/css" media="all">
+
+		<!-- GOOGLE ANALYTICS -->
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-19658334-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
 		<?php wp_head(); ?>
 	</head>
 	<body>

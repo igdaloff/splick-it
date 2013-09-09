958fbff734b134191b3bb75e28526ba05a812281
igdaloff <louis.nathan@gmail.com>
2013-09-09 16:43:27 -0400

    Optimizing images and removing unnecessary ones

--- wp-content/themes/starkers-master/parts/shared/html-header.php
+++ wp-content/themes/starkers-master/parts/shared/html-header.php
@@ -5,10 +5,12 @@
 <!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
 	<head>
-		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
+		<title>Splick-it | Mobile Ordering | Online Ordering System for Restaurants | Branded Apps for Restaurants | Branded ordering apps for restaurants</title>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Splick-it powers custom branded mobile and online ordering apps for restaurants to accept orders from anywhere and provide guests with a VIP experience.">
+		<meta name="keywords" content="online ordering, mobile ordering, custom restaurant apps, personalized restaurant apps, food ordering apps, web ordering, food ordering, restaurant ordering, online ordering system, mobile ordering apps, iphone, android, mobile apps, technology">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
 		<link rel="stylesheet" type="text/css" href="//cloud.typography.com/6961452/726442/css/fonts.css" />

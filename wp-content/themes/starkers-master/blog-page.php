4e580e78e8ca7e857425e0f472d921d6165bec75
igdaloff <louis.nathan@gmail.com>
2013-09-20 14:56:51 -0400

    Fixing blog date so all dates appear - even dupes

--- wp-content/themes/starkers-master/blog-page.php
+++ wp-content/themes/starkers-master/blog-page.php
@@ -28,7 +28,7 @@ $more = 0;
 
 			<article class="standard-section blog-post">
 				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-				<time datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_date(); ?></time>
+				<time datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_time('F j, Y'); ?></time>
 				<?php the_excerpt(); ?><?php array_push($words, ' ... <a href="'. get_permalink() . '">READ MORE</a>');?>
 			</article>
 
